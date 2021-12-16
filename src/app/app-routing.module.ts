0539522637d06f2bb7a657ad8ec18cfa269748d4
d-air1 <dashonwwIII@pm.me>
2021-12-16 15:55:32 -0600

    The registration page now adds the user to the database.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddProductComponent } from './components/add-product/add-product.component';
-import { CartComponent } from './components/cart/cart.component';
-import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { ManageOrderComponent } from './components/manage-order/manage-order.component';
-import { ManageProductsComponent } from './components/manage-products/manage-products.component';
-import { ProfileComponent } from './components/profile/profile.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 
 const routes: Routes = [
   {path:"", redirectTo:"register", pathMatch:"full"},
   {path:"register", component:RegistrationComponent},
   {path:"login", component: LoginComponent},
-  {path:"profile", component: ProfileComponent},
-  {path:"home", component: HomeComponent},
-  {path:"cart", component: CartComponent},
-  {path:"addproduct", component: AddProductComponent},
-  {path:"manageorders", component: ManageOrderComponent},
-  {path:"manageproducts", component: ManageProductsComponent},
   {path:"**", redirectTo:"register", pathMatch:"full"}
 
 ];
--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import {ReactiveMustMatch} from 'src/app/shared/must-match.validator';
+import { GetUsersService } from 'src/app/services/get-users.service';
 
 import { Router } from '@angular/router';
+import { IUserData } from 'src/app/models/user';
+import { HttpParams } from '@angular/common/http';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -12,7 +15,7 @@ export class RegistrationComponent implements OnInit {
   public user: any;
   public isFormSubmitted: boolean;
   public userForm: FormGroup;
-  constructor(private fb: FormBuilder, private router:Router) { 
+  constructor(private fb: FormBuilder, private router:Router, private getUsersService: GetUsersService) { 
     this.user = {};
     this.isFormSubmitted = false;
     this.userForm = {} as FormGroup;
@@ -46,6 +49,34 @@ export class RegistrationComponent implements OnInit {
     if(this.userForm.controls['firstName'].status !== "INVALID" && this.userForm.controls['lastName'].status !== "INVALID" && this.userForm.controls['email'].status !== "INVALID" && this.userForm.controls['pass'].status !== "INVALID" && this.userForm.controls['confirmPass'].status !== "INVALID" && this.userForm.controls['accept'].status !== "INVALID"){
       this.router.navigate(['/login'])
       console.log("yayy");
+
+      // get values from user
+      let email = this.userForm.get('email')!.value;
+      let password = this.userForm.get('pass')!.value;
+      let firstname = this.userForm.get('firstName')!.value;
+      let lastname = this.userForm.get('lastName')!.value;
+    
+      let body = new HttpParams()
+        .set('email', email)
+        .set('password', password)
+        .set('firstName', firstname)
+        .set('lastName', lastname);
+      
+      // get users from json-server
+      this.getUsersService.createUser(body.toString()).toPromise()
+        .then( (data: IUserData[]) => 
+        {
+          if (data !== null)
+          {
+            this.router.navigate(['home']);
+          }  
+          else
+          {
+            console.log(data);
+          }
+        })
+        .catch(message => console.log('Error: ' + message.status + ' ' + message.statusText));
+        
     }
     else{
       alert("Please complete all details");
